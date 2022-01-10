4de83fcc64a962e8fb2d629d749f31aacd7907e3
Kurai Tsukino <sandym3000@hotmail.com>
2022-01-10 00:17:49 -0600

    Experiencia sin estilo

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function index() {
     return (
-        <div>
-            This is Experience
-        </div>
+        <>
+            <h1>Experience</h1>
+
+            <h2>Instituto Mexicano del Seguro Social</h2>
+
+            <h3>Emergency medicine doctor</h3>
+
+            <h4>March 2014 — October 2021</h4>
+
+            <p>Medical attention in an emergency room.</p>
+            <p>Provided thorough pysical evaluations of patients.</p>
+            <p>Work team with nurses, radiological personal and others doctors.</p>
+            <p>Provide a medical report to family members.</p>
+        </>
     )
 }
--- src/components/Experiencia/index.js
+++ src/components/Experiencia/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function index() {
-    return (
-        <div>
-            Esta es Experiencia
-        </div>
-    )
+  return (
+    <>
+      <h1>Experiencia</h1>
+
+      <h2>Instituto Mexicano del Seguro Social</h2>
+
+      <h3>Médica Urgencióloga</h3>
+
+      <h4>Marzo 2014 — Octubre 2021</h4>
+
+      <p>Atención médica en el área de urgencias.</p>
+      <p>Proporcionar evaluaciones físicas exhaustivas a los pacientes.</p>
+      <p>Trabajo en equipo con enfermeras, personal radiológico y otros médicos especialistas.</p>
+      <p>Brindar informe médico a los familiares.</p>
+    </>
+  );
 }
