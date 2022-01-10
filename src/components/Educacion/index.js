045f02b6046dca61e25a5b9a7cbcdf532c5f189f
Kurai Tsukino <sandym3000@hotmail.com>
2022-01-10 00:54:33 -0600

    Educación sin estilos

--- src/components/Educacion/index.js
+++ src/components/Educacion/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function index() {
-    return (
-        <div>
-            Esta es educación
-        </div>
-    )
+  return (
+    <div>
+      <h1>Educación</h1>
+
+      <h2>Ironhack Mexico</h2>
+
+      <h3>Desarrollo Web, Programación</h3>
+
+      <h4>Octubre 2021 - Diciembre 2021</h4>
+
+      <p>Bootcamp de 9 semanas intensivas de Desarrollo Web.</p>
+      <p>Creando proyectos front end y full stack.</p>
+    </div>
+  );
 }
--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -3,7 +3,16 @@ import React from 'react'
 export default function index() {
     return (
         <div>
-            This is education
+            <h1>Education</h1>
+
+            <h2>Ironhack Mexico</h2>
+
+            <h3>Web Development, Computer Programming</h3>
+
+            <h4>October 2021 - December 2021</h4>
+
+            <p>9 week intensive web development bootcamp.</p>
+            <p>Built front end projects and full stack projects.</p>
         </div>
     )
 }
